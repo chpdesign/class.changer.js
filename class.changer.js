2b0f6f9c3ec89ba1eb63a7c91dda2ed56491381e
Gergely Nagy <chpdesign@users.noreply.github.com>
2016-08-23 11:53:34 +0200

    Remove before add!

--- class.changer.js
+++ class.changer.js
@@ -4,31 +4,31 @@ $(window).resize(function(){
 		var w = $(window).innerWidth();
 		if(w < 768 && classer['col-xs'] != undefined)
 		{
-			$(this).addClass(classer['col-xs']);
 			$(this).removeClass(classer['col-sm']);
 			$(this).removeClass(classer['col-md']);
 			$(this).removeClass(classer['col-lg']);
+			$(this).addClass(classer['col-xs']);
 		}
 		else if(w >= 768 && w < 992 && classer['col-sm'] != undefined)
 		{
-			$(this).addClass(classer['col-sm']);
 			$(this).removeClass(classer['col-xs']);
 			$(this).removeClass(classer['col-md']);
 			$(this).removeClass(classer['col-lg']);
+			$(this).addClass(classer['col-sm']);
 		}
-		else if(w <= 992 && classer['col-md'] != undefined)
+		else if(w >= 992 && classer['col-md'] != undefined)
 		{
-			$(this).addClass(classer['col-md']);
 			$(this).removeClass(classer['col-xs']);
 			$(this).removeClass(classer['col-sm']);
 			$(this).removeClass(classer['col-lg']);
+			$(this).addClass(classer['col-md']);
 		}
 		else if(w >= 1200 && classer['col-lg'] != undefined)
 		{
-			$(this).addClass(classer['col-lg']);
 			$(this).removeClass(classer['col-xs']);
 			$(this).removeClass(classer['col-md']);
 			$(this).removeClass(classer['col-md']);
+			$(this).addClass(classer['col-lg']);
 		}
 	});
 });
